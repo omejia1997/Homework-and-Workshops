3ffb0c2d457ced5b06f2e1ef315ebc4f8eff25c9
omejia1997 <omardevaluado08@gmail.com>
2018-11-06 07:51:17 -0500

    ADD Lab 9

---
+++ Workshops/Partial 1/Lab9Operators/Lab9Operatos/src/ec/espe/edu/Lab9Operators/Operation.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.espe.edu.Lab9Operators;
+
+/**
+ *
+ * @author Omar Mejia
+ */
+public class Operation {
+int operand1;
+int operand2;
+int result;
+
+    public int getOperand1() {
+        return operand1;
+    }
+
+    public void setOperand1(int operand1) {
+        this.operand1 = operand1;
+    }
+
+    public int getOperand2() {
+        return operand2;
+    }
+
+    public void setOperand2(int operand2) {
+        this.operand2 = operand2;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+
+public int add(int operand1,int operand2){
+   result = operand1 + operand2;
+   return result;
+}
+    
+}
